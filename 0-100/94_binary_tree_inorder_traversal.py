66834a5f8ec31517f5a141ca4f707571a9e75394
lei8c8 <lei8c8@gmail.com>
2018-08-30 17:45:37 -0400

    0820

---
+++ 0-100/94_binary_tree_inorder_traversal.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        self.helper(root, res)
+        return res
+
+    def helper(self, root, res):
+        current = root
+        if not current:
+            return None
+        self.helper(current.left, res)
+        res.append(current.val)
+        self.helper(current.right, res)
+        
