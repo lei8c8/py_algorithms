1f2297370525436609f662af0736578e37bb9d25
lei8c8 <lei8c8@gmail.com>
2018-09-01 17:19:26 -0400

    0901

--- 400-500/441_arangeing_coins.py
+++ 400-500/441_arangeing_coins.py
@@ -4,16 +4,11 @@ class Solution:
         :type n: int
         :rtype: int
         """
-        if n == 0:
-            return 0
-        sum = 0
-        i = 1
-        while sum <= n:
-            sum += i
-            i += 1
-        return i - 2
+        return int((-1 + (1 + 8 *n) ** 0.5) / 2)
+
+
 
 if __name__ == "__main__":
     solution = Solution()
-    result = solution.arrangeCoins(10)
+    result = solution.arrangeCoins(1)
     print(result)
