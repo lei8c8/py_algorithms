a67ab7d37a9779896ebd3cded67a597913c9f201
lei8c8 <lei8c8@gmail.com>
2018-09-12 21:56:44 -0400

    0912

--- 0-100/8_str_to_integer.py
+++ 0-100/8_str_to_integer.py
@@ -1,24 +1,36 @@
-class Solution:
-    def myAtoi(self, str1):
+class Solution(object):
+    def myAtoi(self, str):
         """
         :type str: str
         :rtype: int
         """
-        try:
-            temp = str1.split()[0]
-        except:
+        temp = str.strip()
+        if not temp:
             return 0
-        if '.' in temp:
-            temp = temp.split('.')[0]
-        try:
-            temp = int(temp)
-        except:
-            temp = 0
-        if temp < 0 - 2 ** 31:
-            temp = 0 - 2 ** 31
-        if temp > 2 ** 31 - 1:
-            temp = 2 ** 31 -1 
-        return temp
+        negative = False
+        resint = 0
+        head = temp[0]
+        if head == "-":
+            negative = True
+        elif head == "+":
+            negative = False
+        elif not head.isnumeric():
+            return 0
+        else:
+            resint += ord(head) - ord('0')
+        for i in range(1,len(temp)):
+            if temp[i].isnumeric():
+                resint = 10*resint + ord(temp[i]) - ord('0')
+                if not negative and resint >= 2147483647:
+                    return 2147483647
+                if negative and resint >= 2147483648:
+                    return -2147483648
+            else:
+                break
+        if not negative:
+            return resint
+        else:
+            return -resint
 
 if __name__ == "__main__":
     solution = Solution()
