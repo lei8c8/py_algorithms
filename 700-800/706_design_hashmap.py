8cd028e9f40e919753414a318bbb22fd14693535
lei8c8 <lei8c8@gmail.com>
2018-09-16 16:18:25 -0400

    0916

--- 700-800/706_design_hashmap.py
+++ 700-800/706_design_hashmap.py
@@ -1,75 +1,16 @@
-
-class Item:
-
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-
-    def __str__(self):
-        return "%s => %s" % (self.key, self.value)
-
-
-class MyHashMap:
+class MyHashMap(object):
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.capacity = 256
-        self.count = 0
-        self.slots = [None for i in range(self.capacity)]
-
-    def _hashIt(self, key):
-        i = 1
-        res = 0
-        for c in str(key):
-            res += i * ord(c)
-            i += 1
-        return res % self.capacity
+        self.items = [0] * 1000000
         
-
     def put(self, key, value):
-        """
-        value will always be non-negative.
-        :type key: int
-        :type value: int
-        :rtype: void
-        """
-        item = Item(key, value)
-        hashValue = self._hashIt(key)
-        while self.slots[hashValue] is not None:
-            if self.slots[hashValue].key == key:
-                break
-            else:
-                hashValue = (hashValue + 1) % self.capacity
-        if self.slots[hashValue] is None:
-            self.count += 1
-        self.slots[hashValue] = item
+        self.items[key] = value + key + 1
         
-
     def get(self, key):
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        :type key: int
-        :rtype: int
-        """
-        hashValue = self._hashIt(key)
-        while self.slots[hashValue] is not None:
-            if self.slots[hashValue].key == key:
-                return self.slots[hashValue].value
-            hashValue = (hashValue + 1) % self.capacity
-        return -1        
-
+        if self.items[key]:
+            return self.items[key] - key - 1
+        else:
+            return -1
+        
     def remove(self, key):
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        :type key: int
-        :rtype: void
-        """
-        hashValue = self._hashIt(key)
-        while self.slots[hashValue] is not None:
-            if self.slots[hashValue].key == key:
-                self.slots[hashValue] = None
-                return
-            hashValue = (hashValue + 1) % self.capacity
-        return    
+        self.items[key] = 0
