2fde6e1e83add064376e8b769414b8e63c27c06c
lei8c8 <lei8c8@gmail.com>
2018-09-18 21:20:49 -0400

    0918

---
+++ 600-700/690_employee_importance.py
@@ -0,0 +1,23 @@
+"""
+# Employee info
+class Employee:
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+"""
+class Solution:
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        hashmap = {employee.id: employee for employee in employees}
+        def imp(employee):
+            return employee.importance + sum(imp(hashmap[sub]) for sub in employee.subordinates)
+        return imp(hashmap[id])
