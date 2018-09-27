7c6dceae3c483b5198feb43c3272a536b464b1ba
lei8c8 <lei8c8@gmail.com>
2018-09-26 21:30:34 -0400

    0926

---
+++ 100-200/153_find_min_in_rotated_sorted_array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums) - 1):
+            if nums[i + 1] < nums[i]:
+                return nums[i+1]
+        return nums[0]
